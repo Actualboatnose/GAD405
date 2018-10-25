021ba10fe3dbd4d02a25ab8787def1a0d5eaefe2
(s) Luke Boatfield <lboatfield@pm7-0077.uopnet.plymouth.ac.uk>
2018-10-25 12:37:50 +0100

    End of second session of creative coding

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -5,19 +5,60 @@
 
 //Initialization function
 function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
+
   console.log("Initialization: OK")
-  createCanvas(1000,1000);
+  createCanvas(700, 400);
 }
 
-//Rendering function
 function draw() {
-  //Set a background color
-  background(255, 255, 0);
 
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
+  background(100);
+
+  strokeWeight(4);
+  stroke(0, 255, 0)
+  line(0, 0, 1200, 700);
+
+  stroke(255, 255, 0)
+  line(700, 0, 0, 390);
+
+  stroke(255, 0, 255)
+  line(10, 10, 700, 10);
+
+  stroke(255, 0, 0)
+  line(690, 10, 690, 690);
+
+  stroke(0, 255, 255)
+  line(10, 10, 10, 700);
+
+  stroke(150, 150, 0)
+  line(0, 390, 700, 390);
+
+  //Center shape position and color
+  fill(255);
+  strokeWeight(1);
+  stroke(0);
+  /*rectMode(CENTER);
+  triangle(350, 100, 500, 250, 200, 250)
+  //Set text preferences for circle0
+  textAlign(CENTER);
+  fill(100);
+  text("Triangle", 350, 175);
+  text("Center Position:", 350, 200);
+  text("X=350, Y=200", width/2, height/2+20);
+  */
+
+  fill(0, 0, 255);
+  ellipse(0, 0, 100, 100);
+
+  fill(255, 0, 0);
+  ellipse(width, 0, 100, 100);
+
+  fill(0, 255, 0);
+  ellipse(0, height, 100, 100);
+
+  fill(255, 255, 0);
+  ellipse(width, height, 100, 100);
+
   console.log("Rendering...")
   noLoop();
 }
