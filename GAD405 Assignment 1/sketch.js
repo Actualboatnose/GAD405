c6fc6372719f5005c59519fb52a55d23ed8b180d
(s) Luke Boatfield <lboatfield@pm7-0077.uopnet.plymouth.ac.uk>
2018-11-22 12:58:14 +0000

    assignment Commit 3

--- GAD405 Assignment 1/sketch.js
+++ GAD405 Assignment 1/sketch.js
@@ -1,41 +1,64 @@
-let size;
-//var colour[0,0,0];
+var input = [];
+var button;
+var r = 255;
+var g = 0;
+var b = 0;
 
 function setup() {
+  input[0] = createInput();
+  input[0].position(45,550);
+  input[1] = createInput();
+  input[1].position(45, 575);
+  input[2] = createInput();
+  input[2].position(45, 600);
+  button = createButton('submit');
+  button.position(input[2].x + input[2].width, 600);
+  button.mousePressed(recolour);
 
+
+  let size;
   createCanvas(windowWidth, windowHeight);
-  frameRate(60);
+  frameRate(30);
   background(random(255));
   //noLoop();
+
 }
 
 function draw()
 {
-  console.log("This is doing things");
-  if (mouseIsPressed)
+  ShapesColour = [random(255),random(255),random(255)];
+  if (input[0].value() == '' || input[1].value() =='' || input[2].value() == '')
   {
-    if (mouseButton === LEFT)
-    {
-      drawTriangle();
-    }
-    else if (mouseButton === CENTER)
-    {
-      drawRectange();
-    }
+    console.log('Insert Values');
   }
   else
   {
-    drawCircle();
+      if (mouseIsPressed)
+    {
+      console.log("This is doing things");
+      if (mouseButton === LEFT)
+      {
+        drawTriangle();
+      }
+      else if (mouseButton === CENTER)
+      {
+        drawRectange();
+      }
+    }
+    else
+    {
+      drawCircle();
+    }
   }
 }
 function drawCircle(x,y)
 {
   strokeWeight(0);
-  fill(random(255),random(255),random(255),127);
+  fill(r,g,b,127);
   ellipse(mouseX, mouseY,mouseX*0.75,mouseY*0.75);
-  fill(random(255),random(255),random(255),127);
+  fill(r,g,b,127);
   ellipse(mouseX, mouseY,mouseX*0.5,mouseY*0.5);
-  fill(random(255),random(255),random(255),127);
+  fill(r,g,b,127);
   ellipse(mouseX, mouseY,mouseX*0.25,mouseY*0.25);
 }
 
@@ -43,13 +66,13 @@ function drawRectange(x,y)
 {
   strokeWeight(0);
   size = random(200);
-  fill(random(255),random(255),random(255),random(255));
+  fill(ShapesColour[0],ShapesColour[1],ShapesColour[2],random(255));
   rect(mouseX/2, mouseY/2,mouseX,mouseY);
-  fill(random(255),random(255),random(255),random(255));
+  fill(ShapesColour[0],ShapesColour[1],ShapesColour[2],random(255));
   rect(mouseX*0.625, mouseY*0.625,mouseX*0.75,mouseY*0.75);
-  fill(random(255),random(255),random(255),random(255));
+  fill(ShapesColour[0],ShapesColour[1],ShapesColour[2],random(255));
   rect(mouseX*0.75, mouseY*0.75,mouseX*0.5,mouseY*0.5);
-  fill(random(255),random(255),random(255),random(255));
+  fill(ShapesColour[0],ShapesColour[1],ShapesColour[2],random(255));
   rect(mouseX*0.875, mouseY*0.875,mouseX*0.25,mouseY*0.25);
 }
 
@@ -57,12 +80,33 @@ function drawTriangle(x,y)
 {
   strokeWeight(0);
   size = random(200);
-  fill(random(255),random(255),random(255),random(255));
+  fill(r,g,b,random(255));
   triangle(mouseX-66, mouseY, mouseX+66, mouseY,mouseX,mouseY/4);
-  fill(random(255),random(255),random(255),random(255));
+  fill(r,g,b,random(255));
   triangle(mouseX-66, mouseY, mouseX+66, mouseY,mouseX,mouseY/3);
-  fill(random(255),random(255),random(255),random(255));
+  fill(r,g,b,random(255));
   triangle(mouseX-66, mouseY, mouseX+66, mouseY,mouseX,mouseY/2);
+}
+
+function recolour()
+{
+  r = input[0].value();
+  g = input[1].value();
+  b = input[2].value();
+
+}
+
+function keyPressed()
+{
+  if (keyCode === 82)
+  {
+    clear();
+    input[0].value('');
+    input[1].value('');
+    input[2].value('');
+    background(random(255));
+  }
+
 }
 
 function windowResized(){
